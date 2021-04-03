1e0e66c05fcb783842377c60efc503e7cfea7c6e
Muhammed <muhammed.bakijev@gmail.com>
2021-04-04 01:17:38 +0200

    Minor changes

--- Main.gd
+++ Main.gd
@@ -2,24 +2,28 @@ extends Node2D
 
 export var DEBUG: bool = true
 
+const GAME_STATES = {active = 0, paused = 1}
+
+var current_state: int
 var _dogecoin_manager: DogecoinManager
 var _clicker_upgrades: Array = []
 var _auto_farmers: Array = []
 
 var cps_formatted_str = "%.8f Doge/s"
 
-onready var coin_counter_label = $GUI/CoinCounterLabel
-onready var cps_label = $GUI/CoinsPerSecondLabel
+onready var coin_counter_label = $GUI/LeftSection/CoinCounterLabel
+onready var cps_label = $GUI/LeftSection/CoinsPerSecondLabel
 onready var auto_farm_timer = $AutoFarmTimer
+onready var scrolling_bg = $GUI/Background/ScrollingBackground
 
 func _ready():
 	_init_signals()
 	
 	_dogecoin_manager = DogecoinManager.new()
 	
-	_auto_farmers.append(AutoFarmer.new(1122.3,2))
-	
 	auto_farm_timer.start()
+	
+	current_state = GAME_STATES.active
 	
 func _process(delta):
 	coin_counter_label.text = str(float(_dogecoin_manager.get_coins()))
@@ -33,7 +37,7 @@ func _add_dogecoins_click():
 				coins_to_add =+ clicker_upgrade.get_calculated_coins(coins_to_add)
 	_dogecoin_manager.add_coins_click(coins_to_add)
 	if DEBUG:
-		print("Added %d coins" % (coins_to_add))
+		print("[INFO] Added %d coins" % (coins_to_add))
 
 
 func _add_dogecoins_auto():
@@ -43,7 +47,7 @@ func _add_dogecoins_auto():
 				var coins_to_add = farmer.get_farmed_coins()
 				_dogecoin_manager.add_coins_click(coins_to_add)
 				if DEBUG:
-					print("AutoFarm added %f coins" % (coins_to_add))
+					print("[INFO] AutoFarm added %f coins" % (coins_to_add))
 
 
 func _calculate_cps():
@@ -53,9 +57,22 @@ func _calculate_cps():
 				cps += farmer.get_farmed_coins()
 	return cps
 
+# Signals
+
 func _init_signals():
 	get_node("DogeClicker").connect("click", self, "_add_dogecoins_click")	
-
-
+	
+	
 func _on_AutoFarmTimer_timeout():
 	_add_dogecoins_auto()
+	
+	
+func _on_MenuButton_pressed():
+	current_state = GAME_STATES.paused
+	scrolling_bg.pause_scrolling = true
+	if DEBUG:
+		print("[INFO] Game state set to PAUSED")
+# Signals end
+
+
+
